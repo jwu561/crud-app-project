72ebc46a30e1975e98c0d84c992a6578149e8fdb
jwu561 <59592585+jwu561@users.noreply.github.com>
2024-04-24 23:22:14 -0400

    fixed time posted ago

--- finalProject/src/components/Homepage.jsx
+++ finalProject/src/components/Homepage.jsx
@@ -4,12 +4,41 @@ import { Link } from 'react-router-dom';
 import './Homepage.css';
 
 function Homepage({ posts }) {
+
+  function timeSince(date) {
+    const seconds = Math.floor((new Date() - date) / 1000);
+    let interval = Math.floor(seconds / 31536000);
+  
+    if (interval > 1) {
+      return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  }
+  
+
+
     return (
         <div className="posts-container">
           {posts.map((post) => (
             <Link key={post.id} to={`/post/${post.id}`} className="post">
               <h2>{post.title}</h2>
-              <p>{post.timestamp.toLocaleString()}</p>
+              <p>Posted {timeSince(new Date(post.timestamp))}</p>
               <p>{post.upvotes} upvotes</p>
             </Link>
           ))}
--- finalProject/src/components/Post.jsx
+++ finalProject/src/components/Post.jsx
@@ -34,12 +34,38 @@ function Post({ posts, setPosts }) {
     navigate('/');
   };
 
+  function timeSince(date) {
+    const seconds = Math.floor((new Date() - date) / 1000);
+    let interval = Math.floor(seconds / 31536000);
+  
+    if (interval > 1) {
+      return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  }
+
   return post ? (
     <div className="post">
       <h2>{post.title}</h2>
       <p>{post.content}</p>
       {post.imageUrl && <img src={post.imageUrl} alt={post.title} />}
-      <p>{post.timestamp.toLocaleString()}</p>
+      <p>Posted {timeSince(new Date(post.timestamp))}</p>
       <button onClick={upvotePost}>👍 {post.upvotes}</button>
       <button onClick={deletePost}>Delete Post</button>
       <button onClick={() => navigate(`/edit/${post.id}`)}>Edit</button>
